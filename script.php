5c1b8747b5da33222fc537c59de279adb206c92e
brian <brianmoraa@gmail.com>
2020-06-27 22:31:30 -0500

    test

--- script.php
+++ script.php
@@ -15,7 +15,7 @@ error_log(print_r($postdata,true));
 $request = json_decode($postdata);
 error_log(print_r($request,true));
 
-$mail->From = $request->email; //remitente
+$mail->setFrom($request->email, $request->name); //remitente
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'tls'; //seguridad
 $mail->Host = "smtp.gmail.com"; // servidor smtp
@@ -23,9 +23,9 @@ $mail->Port = 587; //puerto
 $mail->Username = $request->mailOrigen; //nombre usuario
 $mail->Password = $request->pass; //contraseña
 //destinatario
-$mail->AddAddress($request->email, $request->name);
+$mail->AddAddress($request->mailOrigen);
 $mail->Subject = $request->subject;
-$mail->Body = $request->message;
+$mail->Body = $request->name + ' escribió: ' + $request->message;
 //Avisar si fue enviado o no y dirigir al index
 if ($mail->Send()) {
   echo json_encode(true);
